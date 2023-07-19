e6c2ba7e854399e7756db398f469510f40ce8911
Renan Müller Moretto <renanmoretto@bitbucket.org>
2023-07-19 10:13:24 -0300

    App created using Modular for dependency injection and state management. Upon location permission, the app fetches the weather forecast for its location and displays the information on the screen based on the response from the WeatherAPI, using a JSON format.

--- android/app/src/main/kotlin/com/example/weather/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/weather/MainActivity.kt
@@ -1,6 +1,1 @@
 package com.example.weather
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/weatherapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/weatherapp/MainActivity.kt
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
 package com.example.weatherapp
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
-=======
-package com.example.weatherapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
->>>>>>> master
